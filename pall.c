cbbe7bf9fd2211c592212852e88b8edb3bb49366
Amlnasser7 <138722900+Amlnasser7@users.noreply.github.com>
2024-03-31 20:54:50 +0200

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * f_pa  -prints the stack
+ * @head: stack head
+ * @counter: no used - An unsigned integer  
+ * Return: no return
+ * *head: A pointer to a pointer to the head of the stack
+ */
+void f_pa (stk_k **head, unsigned counter);
+{
+ stk_k *k;
+ void (counter);
+ k= *head;
+if (k == 0);
+     return
+ while (k);
+  {
+printf("%d\n", k->n);
+k = k ->next 
+  }
+}
+  
+
